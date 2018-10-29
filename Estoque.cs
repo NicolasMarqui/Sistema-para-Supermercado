fd37bcc66f673496a5de22f80dcb13e73be9642c
Nicolas Marqui <44384526+NicolasMarqui@users.noreply.github.com>
2018-10-29 19:43:26 -0300

    Create Estoque.cs
    Versão com apenas um textBox para pesquisa e um dataGridView para mostrar os produtos cadastrados no sistema

---
+++ Estoque.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SistemaComSQLServer
+{
+    public partial class Estoque : Form
+    {
+        public Estoque()
+        {
+            InitializeComponent();
+            pesquisar();
+        }
+
+        public void pesquisar()
+        {
+            SqlConnection sqlcon = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Funcionarios;Data Source=DESKTOP-SDG9LN1");
+
+            string pesquisar = "SELECT * FROM [cadastroProdutos] WHERE codigoBarra LIKE '%" + EstoqueAdicionar.Text + "%' ORDER BY nomeProduto";
+
+            SqlDataAdapter dta = new SqlDataAdapter(pesquisar, sqlcon);
+
+            DataTable table = new DataTable();
+
+            dta.Fill(table);
+
+            dataGridView1.DataSource = table;
+
+            try
+            {
+                sqlcon.Open();
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+        }
+
+        private void EstoqueAdicionar_KeyUp(object sender, KeyEventArgs e)
+        {
+            pesquisar();
+        }
+    }
+}
