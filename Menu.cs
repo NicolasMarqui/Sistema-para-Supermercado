79a8a6323f7ca83a54018fe651e4f886c64eb373
Nicolas Marqui <44384526+NicolasMarqui@users.noreply.github.com>
2018-10-29 19:34:57 -0300

    Update Menu.cs
    Adicionado resto das telas, menu de settings para trocar a cor do sistema e fazer logout

--- Menu.cs
+++ Menu.cs
@@ -32,7 +32,17 @@ namespace SistemaComSQLServer
 
             mostrarNome.Text = Login.nomeMenu;
 
-            
+            if (checkBox1.Checked)
+            {
+                CadProdutos cad = new CadProdutos();
+                cad.FormBorderStyle = FormBorderStyle.None;
+                cad.BackColor = Color.Black;
+            }
+            else
+            {
+                CadProdutos cad = new CadProdutos();
+                cad.FormBorderStyle = FormBorderStyle.Sizable;
+            }
         }
 
         private void timerRelogio_Tick(object sender, EventArgs e)
@@ -46,11 +56,103 @@ namespace SistemaComSQLServer
             Application.Exit();
         }
 
-        private void logout_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AlterarProd alt = new AlterarProd();
+            alt.ShowDialog();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Left = Top = 0;
+            Width = Screen.PrimaryScreen.WorkingArea.Width;
+            Height = Screen.PrimaryScreen.WorkingArea.Height;
+            label1.Font = new Font("Arial", 62);
+            label2.Font = new Font("Arial", 30);
+            label1.Left = 670;
+            label2.Left = 760;
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            CadProdutos cadProd = new CadProdutos();
+            cadProd.ShowDialog();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            panelConfig.Visible = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                
+
+                this.BackColor = Color.FromArgb(37, 46, 59);
+                this.ForeColor = Color.White;
+                panelFechar.BackColor = Color.FromArgb(220, 20, 60);
+                panelFechar.ForeColor = Color.Black;
+
+                panelNavBar.BackColor = Color.FromArgb(220, 20, 60);
+                panelNavBar.ForeColor = Color.Black;
+
+                panelNome.BackColor = Color.FromArgb(220, 20, 60);
+                panelNome.ForeColor = Color.Black;
+
+                label1.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+
+                btnLog.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.BackColor = Color.White;
+                this.ForeColor = Color.Black;
+                panelFechar.BackColor = Color.FromArgb(13, 93, 142);
+                panelFechar.ForeColor = Color.White;
+
+                panelNavBar.BackColor = Color.FromArgb(13, 93, 142);
+                panelNavBar.ForeColor = Color.Black;
+
+                panelNome.BackColor = Color.FromArgb(13, 93, 142);
+                panelNome.ForeColor = Color.White;
+
+                label1.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            panelConfig.Hide();
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
         {
             Login login = new Login();
             this.Hide();
             login.Show();
+        }
+
+        private void btnEstoque_Click(object sender, EventArgs e)
+        {
+            Estoque estoque = new Estoque();
+            estoque.ShowDialog();
+        }
+
+        private void BtnVoltarNormal_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnCaixa_Click(object sender, EventArgs e)
+        {
+            Caixa caixa = new Caixa();
+            caixa.ShowDialog();
         }
     }
 }
