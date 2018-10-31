16e025e942b500c115b5f825afb89570153409ac
Nicolas Marqui <44384526+NicolasMarqui@users.noreply.github.com>
2018-10-31 07:18:16 -0300

    Update Caixa.cs
    Caixa funcionando - Adicionar item no carrinho, input pedindo a quantidade do item, label  mostrando o total da compra, double click para remover o item, falta adicionar desconto na compra de acordo com o metodo de pagamento e dar baixa no estoque

--- Caixa.cs
+++ Caixa.cs
@@ -13,6 +13,9 @@ namespace SistemaComSQLServer
 {
     public partial class Caixa : Form
     {
+        public static decimal valorTelaPagamento = 0;
+        decimal soma;
+
         public Caixa()
         {
             InitializeComponent();
@@ -52,30 +55,119 @@ namespace SistemaComSQLServer
             pesquisar();
         }
 
+        string quantidadeTabelaUm;
+        string quantidadeTabelaUm2;
+
         private void dataGridView1_DoubleClick(object sender, EventArgs e)
         {
-            MessageBox.Show("add");
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Nome do Produto");
-            dt.Columns.Add("Quantidade");
-            dt.Columns.Add("Preço");
+            decimal total = 0;
+            decimal result = 0;
+            soma = 0;
 
-            //foreach(DataGridViewRow row in dataGridView1.Rows)
-            //{
-
-            for(int i = 0;i < dataGridView1.Rows.Count; i++)
+            try
             {
-                dt.Rows.Add();
-                int rowIndex = dataGridView1.CurrentCell.RowIndex;
-                int columnIindex = dataGridView1.CurrentCell.ColumnIndex;
-                dt.Rows.Add(dataGridView1.Rows[rowIndex].Cells[columnIindex].Value);
-            }
-                /*int rowIndex = dataGridView1.CurrentCell.RowIndex;
-                int columnIindex = dataGridView1.CurrentCell.ColumnIndex;
-                dt.Rows.Add(dataGridView1.Rows[rowIndex].Cells[columnIindex].Value);
-            //}*/
+                /*dataGridView2.DataSource = dataGridView1;
+                dataGridView2.DataSource = null;
+                dataGridView2.Rows.Clear();*/
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+                {
+                    string quantidadeInput = InputDialogBox.Show("Quantidade");
+                    quantidadeTabelaUm = row.Cells[3].Value.ToString();
+                    label3.Text = Convert.ToInt32(quantidadeInput).ToString();
 
-            dataGridView2.DataSource = dt;
+                    string nomeDataDois = row.Cells[2].Value.ToString();
+                    string precoDataDois = row.Cells[10].Value.ToString();
+                    row.Cells[3].Value = quantidadeInput.ToString();
+                    string quantidadeData2 = row.Cells[3].Value.ToString();
+
+                    dataGridView2.Rows.Add(nomeDataDois, quantidadeData2, precoDataDois);
+                    labelTotalItem.Text = precoDataDois.ToString();
+                    labelQuantidade.Text = quantidadeInput.ToString();
+
+                }
+                
+                for(int i = 0;i < dataGridView2.Rows.Count;i++)
+                {
+                    decimal teste = Convert.ToDecimal(dataGridView2.Rows[i].Cells[1].Value) * Convert.ToDecimal(dataGridView2.Rows[i].Cells[2].Value);
+                    soma += teste;
+                }
+
+                labelTotalCompra.Text = soma.ToString();
+                valorTelaPagamento = Convert.ToDecimal(labelTotalCompra.Text);
+
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                for(int i = 0;i < dataGridView1.Rows.Count;i++)
+                {
+                    dataGridView2.DataSource = row.Cells[i].Value;
+                }
+            }
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            Pagamento pagar = new Pagamento();
+            pagar.ShowDialog();
+
+            SqlConnection sqlcon = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Funcionarios;Data Source=DESKTOP-SDG9LN1");
+
+            string update = "UPDATE cadastroProdutos SET quantidade = @quantidade";
+
+            SqlCommand cmd = new SqlCommand(update, sqlcon);
+
+            cmd.Parameters.AddWithValue("@quantidade", (Convert.ToInt32(label3.Text) - 15));
+
+            try
+            {
+                sqlcon.Open();
+                cmd.ExecuteNonQuery();
+                //MessageBox.Show("Produto atualizado carai");
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            finally
+            {
+                sqlcon.Close();
+            }
+
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Clique duas vezes em cima de uma linha para excluir o produto");
+        }
+
+        private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        decimal id,id2;
+
+        private void dataGridView2_DoubleClick(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow row in dataGridView2.SelectedRows)
+            {
+                id = Convert.ToDecimal(row.Cells[2].Value);
+                id2 = Convert.ToDecimal(row.Cells[1].Value);
+                dataGridView2.Rows.RemoveAt(row.Index);
+
+                soma -= (Convert.ToDecimal(id)) * Convert.ToDecimal(id2);
+            }
+
+            labelTotalCompra.Text = soma.ToString();
+            valorTelaPagamento = Convert.ToDecimal(labelTotalCompra.Text);
         }
     }
 }
