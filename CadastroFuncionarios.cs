5c39273258b996a2a946436eced0e26c4302b6c3
Nicolas Marqui <44384526+NicolasMarqui@users.noreply.github.com>
2018-11-29 01:38:17 -0200

    Update CadastroFuncionarios.cs

--- CadastroFuncionarios.cs
+++ CadastroFuncionarios.cs
@@ -8,126 +8,138 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace SistemaComSQLServer
 {
-    public partial class CadProdutos : Form
+    public partial class CadFuncionario : Form
     {
-        public CadProdutos()
+        public CadFuncionario()
         {
             InitializeComponent();
-            mostrar();
-            //this.StartPosition = FormStartPosition.Manual;
-            //this.Location = new Point(400,180);
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(400, 180);
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-        }
-
-        private void btnCadProd_Click(object sender, EventArgs e)
+        private void btnCadastarFuncionario_Click(object sender, EventArgs e)
         {
             SqlConnection sqlcon = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Funcionarios;Data Source=DESKTOP-SDG9LN1");
+            
+            string addFuncionario = "INSERT INTO [Login](username,password,nivel) VALUES (@username,@password,@nivel)";
 
-            string SalvarProduto = "INSERT INTO [cadastroProdutos](codigoBarra,nomeProduto,grupo,subGrupo,embalagem,quantidade,fabricante,CNPJ,precoCompra,precoVenda) VALUES (@codigoBarra,@nomeProduto,@grupo,@subGrupo,@embalagem,@quantidade,@fabricante,@CNPJ,@precoCompra,@precoVenda)";
-
-            SqlCommand cmd = new SqlCommand(SalvarProduto, sqlcon);
-
-            cmd.Parameters.AddWithValue("@codigoBarra", CodigoBarraCad.Text);
-            cmd.Parameters.AddWithValue("@nomeProduto", nomeCad.Text);
-            cmd.Parameters.AddWithValue("@grupo", GrupoCad.SelectedItem);
-            cmd.Parameters.AddWithValue("@subGrupo", SubGrupoCad.SelectedItem);
-            cmd.Parameters.AddWithValue("@embalagem", EmbalagemCad.Text);
-            cmd.Parameters.AddWithValue("@quantidade", QuantidadeCad.Value);
-            cmd.Parameters.AddWithValue("@fabricante", FabricanteCad.Text);
-            cmd.Parameters.AddWithValue("@CNPJ", CNPJCad.Text);
-            cmd.Parameters.AddWithValue("@precoCompra", PrecoCompraCad.Text);
-            cmd.Parameters.AddWithValue("@precoVenda", PrecoVendaCad.Text);
-
-            try
-            {
-                sqlcon.Open();
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Produto Cadastrado com Sucesso");
-                labelAviso.Visible = true;
-                //CodigoBarraCad.Clear();
-
-            }
-            catch(Exception E)
-            {
-                MessageBox.Show(E.Message);
-            }
-            finally
-            {
-                sqlcon.Close();
-            }
-        }
-
-        private void GrupoCad_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            SubGrupoCad.Items.Clear();
-
-            if (GrupoCad.SelectedItem.ToString() == "LIMPEZA")
-            {
-                SubGrupoCad.SelectedItem = "";
-                SubGrupoCad.Items.Add("DESINFETANTE");
-                SubGrupoCad.Items.Add("DESINFETANTE");
-                SubGrupoCad.Items.Add("DESINFETANTE");
-                SubGrupoCad.Items.Add("DESINFETANTE");
-            }else if(GrupoCad.SelectedItem.ToString() == "COMIDA")
-            {
-                SubGrupoCad.SelectedItem = "";
-                SubGrupoCad.Items.Add("BOLACHA");
-                SubGrupoCad.Items.Add("BOLACHA");
-                SubGrupoCad.Items.Add("BOLACHA");
-                SubGrupoCad.Items.Add("BOLACHA");
-            }
-        }
-
-        private void CadProdutos_Load(object sender, EventArgs e)
-        {
-            GrupoCad.Items.Add("LIMPEZA");
-            GrupoCad.Items.Add("COMIDA");
-            GrupoCad.Items.Add("CONGELADOS");
-            GrupoCad.Items.Add("MACONHA");
-            mostrar();
-            dataGridView1.Refresh();
-
-        }
-      
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        void mostrar()
-        {
-            SqlConnection sqlcon = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Funcionarios;Data Source=DESKTOP-SDG9LN1");
-
-            string mostrar = "SELECT * FROM [cadastroProdutos]";
-
-            SqlDataAdapter dta = new SqlDataAdapter(mostrar, sqlcon);
+            //Antes de adicionar verifica se já possui um funcionario com o mesmo username cadastrado
+            string checkSeJaPossui = "SELECT * FROM [Login] WHERE username = '" + UserCadastro.Text + "'";
+            SqlDataAdapter dta = new SqlDataAdapter(checkSeJaPossui, sqlcon);
 
             DataTable table = new DataTable();
 
             dta.Fill(table);
 
-            dataGridView1.DataSource = table;
+            if(table.Rows.Count > 0)
+            {
+                MessageBox.Show("Usuário já registrado..");
+            }
 
-            try
+            //Se não possuir nenhum , continua com o cadastro
+            if (string.IsNullOrEmpty(UserCadastro.Text))
             {
-                sqlcon.Open();
-            }
-            catch(Exception E)
+                MessageBox.Show("O nome do funcionário não pode ser vazio","ERRO",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }else if (string.IsNullOrEmpty(SenhaCadastro.Text))
             {
-                MessageBox.Show(E.Message);
-            }
-            finally
+                MessageBox.Show("A senha não pode ser vazia", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }else if (string.IsNullOrEmpty(NivelCadastro.Text))
             {
-                sqlcon.Close();
+                MessageBox.Show("O nivel não pode ser vazio", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            else
+            {
+                SqlCommand cmd = new SqlCommand(addFuncionario, sqlcon);
+
+                cmd.Parameters.AddWithValue("@username", UserCadastro.Text);
+                cmd.Parameters.AddWithValue("@password", SenhaCadastro.Text);
+                cmd.Parameters.AddWithValue("@nivel", NivelCadastro.Text);
+
+                //SqlParameter imagem = new SqlParameter("@img", SqlDbType.Binary);
+                try
+                {
+                    sqlcon.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Funcionário Cadastrado com sucesso");
+                    UserCadastro.Clear();
+                    SenhaCadastro.Clear();
+                    NivelCadastro.Clear();
+                    //pictureBox1.Image = null;
+
+
+                }
+                catch (Exception E)
+                {
+                    MessageBox.Show(E.Message);
+                }
+                finally
+                {
+                    sqlcon.Close();
+                }
+            }
+        }
+
+        /*Bitmap bmp;
+        byte[] foto;
+
+        private void btnBuscarImagem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string caminho = openFileDialog1.FileName;
+
+                bmp = new Bitmap(caminho);
+
+                pictureBox1.Image = bmp;
+            }
+
+            MemoryStream memory = new MemoryStream();
+
+            bmp.Save(memory, ImageFormat.Bmp);
+
+            foto = memory.ToArray();
+        }*/
+
+        private void fecharCad_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void checkMostrarSenhaCad_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkMostrarSenhaCad.Checked)
+            {
+                SenhaCadastro.PasswordChar = '\0';
+            }
+            else
+            {
+                SenhaCadastro.PasswordChar = '*';
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (panel1.Visible)
+            {
+                panel1.Visible = false;
+            }
+            else
+            {
+                panel1.Visible = true;
+            }
+        }
+
+        private void btnVerTodos_Click(object sender, EventArgs e)
+        {
+            TodosFuncionarios todos = new TodosFuncionarios();
+            todos.ShowDialog();
         }
     }
 }
-
